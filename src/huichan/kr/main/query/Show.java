2df7757e958727379819fc9cb89790f053c7a5ae
Huichan Bak <hc.bak@outlook.com>
2024-08-18 11:57:01 +0900

    feat: TABLE에 대한 SHOW 기능 추가

--- src/huichan/kr/main/query/Show.java
+++ src/huichan/kr/main/query/Show.java
@@ -3,17 +3,24 @@ package huichan.kr.main.query;
 import java.io.File;
 
 public class Show {
-    public static void query(String type) {
+    public static void query(String type, String useDB) {
         switch (type.toUpperCase()) {
             case "DATABASES":
-                File[] fileList = new File("database").listFiles();
-                if (fileList == null)
+                File[] databaseList = new File("database").listFiles();
+                if (databaseList == null)
                     System.out.println("데이터베이스가 없습니다.");
                 else
-                    for (File value : fileList) { System.out.println(value.getName()); }
+                    for (File value : databaseList) { System.out.println(value.getName()); }
                 break;
-        
+            case "TABLES":
+                File[] tableList = new File("database\\" + useDB).listFiles();
+                if (tableList == null)
+                    System.out.println("테이블이 없습니다.");
+                else
+                    for (File value : tableList) { System.out.println(value.getName().substring(0, value.getName().lastIndexOf('.'))); }
+                break;
             default:
+                System.out.println("지원하지 않는 SHOW 쿼리입니다.");
                 break;
         }
     }
